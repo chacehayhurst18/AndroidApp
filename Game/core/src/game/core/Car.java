21fbd47792da38336df4d4e3413b2e8df22530e6
jdoolittle126 <jdoolittle126@gmail.com>
2017-04-03 14:12:18 -0400

    Yeet

---
+++ Game/core/src/game/core/Car.java
@@ -0,0 +1,8 @@
+package game.core;
+
+import com.badlogic.ashley.core.Component;
+
+public class Car extends Component {
+	
+
+}
--- Game/core/src/game/core/MyGame.java
+++ Game/core/src/game/core/MyGame.java
@@ -1,12 +1,13 @@
-package com.mygdx.game;
+package game.core;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class MyGame extends ApplicationAdapter {
+public class MyGame extends Game {
 	SpriteBatch batch;
 	Texture img;
 	
